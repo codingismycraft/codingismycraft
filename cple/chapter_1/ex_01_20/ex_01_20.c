525d0079e0ec47d34c755dbee6472c4faac3b8f9
jpazarzis <jpazarzis@gmail.com>
2020-10-05 20:59:18 -0400

    Adds ex_01_20.c.

---
+++ cple/chapter_1/ex_01_20/ex_01_20.c
@@ -0,0 +1,33 @@
+/* Write a program that replaces tabs in the input with the proper number
+of blanks to space to the next tab stop.
+
+to build: gcc -g ex_01_20.c -o ex_01_20
+to test: cat text | ex_01_20
+*/
+#include <stdio.h>
+
+#define TAB_LENGTH 18
+
+
+int main() {
+    int c;
+    int length = 0;
+    int i, j;
+
+    while( (c=getchar()) != EOF ){
+        if (c == '\t') {
+          j =  TAB_LENGTH - length;
+          for(i = 0; i <j; ++i)
+            putchar(' ');
+            length = 0;
+        }
+        else if (c == '\n') {
+            putchar(c);
+            length = 0;
+        }
+        else {
+            putchar(c);
+            length += 1;
+        }
+    }
+}
