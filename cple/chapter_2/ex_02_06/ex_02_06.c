efc410b432f5ec05224706304fcba2622164be34
jpazarzis <jpazarzis@gmail.com>
2020-10-11 01:25:44 -0400

    Adds ex_02_06.

--- cple/chapter_2/ex_02_06/ex_02_06.c
+++ cple/chapter_2/ex_02_06/ex_02_06.c
@@ -1,5 +1,4 @@
-/*
-Write the function setbits(x, p, n, y) that returns x with the n
+/* Write the function setbits(x, p, n, y) that returns x with the n
 bits that begin at position p set to the rightmost n bits of y,
 leaving the other bits unchanged.
 
