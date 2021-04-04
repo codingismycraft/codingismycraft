beed6603d07f3afd95131f78cfa8651d7f3e54f4
jpazarzis <jpazarzis@rapidsos.com>
2021-04-04 12:16:03 -0400

    Refactors sorting_large_file.

---
+++ algorithms/sorting_large_file/make_files_to_merge.py
@@ -0,0 +1,67 @@
+"""Utility to create a large amount of files to sort.
+
+The files created from this utility simulate the breakdown of a huge
+file to smaller sorted files exactly as it happens in the related
+algorithm.
+
+Doing so allows us to concentrate on the file merging algorithm and
+relieves us from the details that have to do with the breaking of the
+huge file to smaller sorted files.  Also it allows us to creating our
+testing data in a self-sufficient manner without having to rely on
+an external source where to download the "huge" file from.
+"""
+
+import faker
+import os
+
+SORTED_FILE_NAME_MASK = "./sorted_files/sorted_{index}".format
+NUMBER_OF_LINES_PER_FILE = 10000
+MAX_LINE_LENGTH = 70
+NUMBER_OF_FILES_TO_CREATE = 500
+
+fake_gen = faker.Faker()
+
+
+def make_file(file_index, count):
+    """Makes a file containing random data.
+
+    :param int file_index: The "index" of the file to create.
+    :param int count: The number of rows to add to the file.
+
+    :returns: The number of lines in the file that was created.
+    :rtype: int
+    """
+    filename = SORTED_FILE_NAME_MASK(index=file_index)
+    print(f"Creating: {filename}")
+    lines = [
+        fake_gen.text(MAX_LINE_LENGTH).lower().replace(' ', '')
+        for _ in range(count)
+    ]
+    lines.sort()
+    with open(filename, 'w') as f:
+        for line in lines:
+            f.write(f'{line}\n')
+    return len(lines)
+
+
+def make_all(count):
+    """Makes the 'smaller' sorted files.
+
+    :param int count: The number of "smaller and sorted" files to create.
+    """
+    os.system("mkdir -p ./sorted_files")
+    os.system("mkdir -p ./solution")
+    os.system("rm ./sorted_files/*")
+    os.system("rm ./solution/*")
+    total_lines = 0
+    total_files_created = 0
+    for index in range(1, count + 1):
+        total_lines += make_file(index, NUMBER_OF_LINES_PER_FILE)
+        total_files_created += 1
+    print(f"File created: {total_files_created}\n"
+          f"Total number of lines: {total_lines}")
+
+
+if __name__ == '__main__':
+    print(f"Creating {NUMBER_OF_FILES_TO_CREATE} sorted files.")
+    make_all(NUMBER_OF_FILES_TO_CREATE)
--- algorithms/sorting_large_file/sort_large_file.py
+++ algorithms/sorting_large_file/sort_large_file.py
@@ -1,97 +1,60 @@
 import datetime
-import os
 import glob
-
-import file_wrapper
 import heap as heap_lib
 
-FileWrapper = file_wrapper.FileWrapper
-
-LARGE_FILE_NAME = 'large.txt'
-LINES_PER_FILE = 2000
-SORTED_FILENAME_MASK = 'sort_data/sorted'
+SORTED_FILENAME_MASK = './sorted_files/sorted'
+OUTPUT_FILE = './solution/sorted.txt'
+OUTPUT_FILE_FROM_MERGE = './solution/sorted_with_merge.txt'
 
 MinHeap = heap_lib.MinHeap
 
 
-def delete_sorting_files():
-    # Iterate over the list of filepaths & remove each file.
-    for filePath in glob.glob(f"{SORTED_FILENAME_MASK}_*.txt"):
-        try:
-            print(f"Deleting: {filePath}")
-            os.remove(filePath)
-        except OSError:
-            print("Error while deleting file")
+class FileWrapper:
+    def __init__(self, filename):
+        self._filename = filename
+        self._file = open(filename)
+        self._current_line = self._file.readline()
+
+    def __lt__(self, other):
+        return self._current_line < other._current_line
+
+    def __le__(self, other):
+        return self._current_line <= other._current_line
+
+    def __repr__(self):
+        return self._filename
+
+    @property
+    def current_line(self):
+        if self.reached_end:
+            return None
+        line = self._current_line
+        line = line.strip()
+        self._current_line = self._file.readline()
+        return line
+
+    @property
+    def reached_end(self):
+        return self._current_line is None or len(
+            self._current_line.strip()) == 0
 
 
-def break_down_file(filename, lines_per_file):
-    delete_sorting_files()
-    lines = []
-    file_index = 0
-    with open(filename) as input_file:
-        for line in input_file.readlines():
-            lines.append(line)
-            if len(lines) >= lines_per_file:
-                file_index += 1
-                output_file_name = f"{SORTED_FILENAME_MASK}_{file_index}.txt"
-                with open(output_file_name, 'w') as output_file:
-                    output_file.writelines(lines)
-                lines = []
-    sort_all_files()
+def check_sorted_subfiles():
+    mask = f"{SORTED_FILENAME_MASK}_*"
 
-
-def sort_file(filename):
-    lines = []
-    with open(filename) as f:
-        for line in f.readlines():
-            lines.append(line)
-    lines.sort()
-    with open(filename, 'w') as f:
-        for line in lines:
-            f.write(line)
-
-
-def sort_all_files():
-    for filename in glob.glob(f"{SORTED_FILENAME_MASK}*.txt"):
-        print(f"Sorting: {filename}")
-        sort_file(filename)
-
-
-def merge_using_qsort():
-    print("Sorting using qsort")
-    output_filename = f"{SORTED_FILENAME_MASK}*.txt"
-    file_wrappers = [
-        FileWrapper(filename)
-        for filename in glob.glob(output_filename)
-    ]
-    started = datetime.datetime.now()
-    with open(f"{SORTED_FILENAME_MASK}.txt", 'w') as output:
-        counter = 0
-        while file_wrappers:
-            counter += 1
-            if counter % 100000 == 0:
-                print(counter)
-            file_wrappers.sort()
-            first_wrapper = file_wrappers[0]
-            line = first_wrapper.current_line
-            output.write(line)
-            output.write('\n')
-            if first_wrapper.reached_end:
-                file_wrappers.remove(first_wrapper)
-    finished = datetime.datetime.now()
-    duration = (finished - started).total_seconds()
-    print(f'duration: {duration}')
-    return duration
+    for filename in glob.glob(mask):
+        print(f'checking {filename}')
+        assert check_file_sorted(filename)
 
 
 def merge_using_heap():
     print("Sorting using heap")
     heap = MinHeap()
-    for filename in glob.glob(f"{SORTED_FILENAME_MASK}_*.txt"):
+    mask = f"{SORTED_FILENAME_MASK}_*"
+    for filename in glob.glob(mask):
         heap.add(FileWrapper(filename))
-
     started = datetime.datetime.now()
-    with open(f"{SORTED_FILENAME_MASK}.txt", 'w') as output:
+    with open(OUTPUT_FILE_FROM_MERGE, 'w') as output:
         counter = 0
         while len(heap) > 0:
             counter += 1
@@ -120,14 +83,5 @@ def check_file_sorted(filename):
 
 
 if __name__ == '__main__':
-    break_down_file(LARGE_FILE_NAME, LINES_PER_FILE)
-
-    heap_duration = merge_using_heap()
-    assert check_file_sorted(f'{SORTED_FILENAME_MASK}.txt')
-
-    qsort_duration = merge_using_qsort()
-    assert check_file_sorted(f'{SORTED_FILENAME_MASK}.txt')
-
-    print(f'heap_duration: {heap_duration}')
-    print(f'qsort_duration: {qsort_duration}')
-    # delete_sorting_files()
+    merge_using_heap()
+    assert check_file_sorted(OUTPUT_FILE_FROM_MERGE)
