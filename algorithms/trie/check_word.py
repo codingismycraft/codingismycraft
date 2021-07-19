2d1b0a0b0dfe88264d95a3936a335164ede97971
jpazarzis <jpazarzis@rapidsos.com>
2021-07-18 23:06:31 -0400

    Adds comments to Trie.

--- algorithms/trie/check_word.py
+++ algorithms/trie/check_word.py
@@ -1,13 +1,22 @@
-import trie
-import datetime
+"""Simple autocompletion utility using Trie."""
 
-# Aliases
+import trie
+
+# Aliases.
 Trie = trie.Trie
+_WORDS_FILE_NAME = "/vagrant/algorithms/trie/dictionary"
 
 
 def make_root():
+    """Makes the root Trie.
+
+    Loads the root trie with all the words contained in the dictionary.
+
+    :return: The root Trie node.
+    :rtype: Trie.
+    """
     words = []
-    with open("/vagrant/algorithms/trie/dictionary") as f:
+    with open(_WORDS_FILE_NAME) as f:
         for line in f:
             word = line.strip().lower()
             if word:
@@ -18,13 +27,26 @@ def make_root():
     return root
 
 
-def make_suggestion(prefix):
+def make_suggestion(prefix, root):
+    """Finds the possible suggestions for the passed in prefix.
+
+    :param str prefix: The prefix lookup.
+    :param Trie root: The root trie to use.
+
+    :return: The suggestions for the passed in prefix.
+    :rtype: list [str]
+    """
+    prefix = prefix.strip().lower()
     print(root.get_suggestions(prefix))
 
 
-if __name__ == '__main__':
+def main():
+    """Receives input from used and displays the matches."""
     root = make_root()
-
     while True:
         prefix = input("Enter prefix: ")
-        make_suggestion(prefix)
+        make_suggestion(prefix, root)
+
+
+if __name__ == '__main__':
+    main()
