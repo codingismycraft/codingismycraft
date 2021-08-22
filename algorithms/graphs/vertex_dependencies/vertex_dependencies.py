1ea28ff2fffc881d04b6e1060a8a50447444065d
jpazarzis <jpazarzis@rapidsos.com>
2021-08-22 03:48:38 -0400

    Makes vertex dependencies non recursive.

--- algorithms/graphs/vertex_dependencies/vertex_dependencies.py
+++ algorithms/graphs/vertex_dependencies/vertex_dependencies.py
@@ -40,3 +40,42 @@ class DependencyCounter:
         }
 
         return dependency_count
+
+
+def count_dependencies(graph):
+    nodes = list(graph.keys())
+    dependencies = {}
+    for node in nodes:
+        _traverse_node(graph, node, dependencies)
+    return {k: len(v) for k, v in dependencies.items()}
+
+
+def _traverse_node(graph, node, dependencies):
+    visited = set()
+    stack = []
+    current_node = node
+    while current_node:
+        if node not in dependencies:
+            dependencies[node] = set()
+        for child in graph[current_node]:
+            dependencies[node].add(child)
+        if current_node not in visited:
+            visited.add(current_node)
+        next_node = _get_unvisited_child(current_node, graph, visited)
+        if next_node is not None:
+            stack.append(current_node)
+            current_node = next_node  # Move forward.
+        elif stack:
+            current_node = stack.pop()  # Move Backward.
+        else:
+            current_node = None
+    return dependencies
+
+
+def _get_unvisited_child(node, graph, visited):
+    for child in graph[node]:
+        if child not in visited:
+            return child
+    return None
+
+
--- algorithms/graphs/vertex_dependencies/vertex_dependencies_test.py
+++ algorithms/graphs/vertex_dependencies/vertex_dependencies_test.py
@@ -10,6 +10,69 @@ DependencyCounter = vertex_dependencies.DependencyCounter
 class TestVertexDependencies(unittest.TestCase):
     """Tests the vertex_dependencies module."""
 
+    def test_traverse_node(self):
+        """Tests the traverse_node function."""
+        data = {
+            "A": ["B", "C", "D"],
+            "B": [],
+            "C": ["E"],
+            "D": ["E"],
+            "E": [],
+            "F": ["K"],
+            "G": ["H", "I"],
+            "H": ["I"],
+            "I": ["K"],
+            "K": [],
+            "L": []
+        }
+
+        expected = {
+            "A": 4,
+            "B": 0,
+            "C": 1,
+            "D": 1,
+            "E": 0,
+            "F": 1,
+            "G": 3,
+            "I": 1,
+            "H": 2,
+            "K": 0,
+            "L": 0
+        }
+        retrieved = vertex_dependencies.count_dependencies(data)
+        self.assertEqual(len(retrieved), len(expected))
+        for k, v in retrieved.items():
+            self.assertEqual(v, expected[k])
+
+    def test_traverse_node2(self):
+        """Tests the make_weakly_connected."""
+        data = {
+            "1": ["2", "3"],
+            "2": ["6"],
+            "3": ["4"],
+            "4": ["5"],
+            "5": ["6"],
+            "6": []
+        }
+
+        expected = {
+            "1": 5,
+            "2": 1,
+            "3": 3,
+            "4": 2,
+            "5": 1,
+            "6": 0
+        }
+
+        retrieved = vertex_dependencies.count_dependencies(data)
+
+        self.assertEqual(len(retrieved), len(expected))
+
+        for k, v in retrieved.items():
+            self.assertEqual(v, expected[k])
+
+
+
     def test_count_dependencies(self):
         """Tests the make_weakly_connected."""
         data = {
