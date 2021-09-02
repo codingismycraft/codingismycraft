38a34d4f3dbbeac740dd5265c9c98ba26bf200ae
jpazarzis <jpazarzis@rapidsos.com>
2021-09-01 20:08:00 -0400

    Uses the union find in Kruskal.

--- algorithms/graphs/kruskal/kruskal.py
+++ algorithms/graphs/kruskal/kruskal.py
@@ -1,86 +1,6 @@
 """Implements the Kruskal's min path Spanning tree."""
 
 
-class DisjointSet:
-    """Implements disjoint set.
-
-    :ivar dict _nodes: Holds a dict from the node name to its parent or if
-    it is a root to the number of its children (including self).
-    """
-
-    def __init__(self):
-        """Initializer."""
-        self._nodes = {}
-
-    def check_cycle(self, node1, node2):
-        """Checks is adding the passed in edge will form an edge.
-
-        :param node1: The first node of the edge to check.
-        :param node2: The second node of the edge to check.
-
-        :return: True if adding the edge will form a cycle.
-        :rtype: bool
-        """
-        return self._find_parent(node1) == self._find_parent(node2)
-
-    def count_disjoint_sets(self):
-        """Counts the number of disjoint sets.
-
-        :return: The number of disjoint sets.
-        :rtype: int
-        """
-        count = 0
-        for value in self._nodes.values():
-            if isinstance(value, int):
-                count += 1
-        return count
-
-    def union(self, node1, node2):
-        """Unions the passed in edge if no cycle is created.
-
-        :param node1: The first node of the edge to add.
-        :param node2: The second node of the edge to add.
-
-        :return: True if adding the edge was added.
-        :rtype: bool
-        """
-        p1 = self._find_parent(node1)
-        p2 = self._find_parent(node2)
-
-        if p1 == p2:
-            return False  # Forms a cycle.
-
-        # The parent will be the "heavier" node.
-        if self._nodes[p1] >= self._nodes[p2]:
-            parent = p1
-            child = p2
-        else:
-            parent = p2
-            child = p1
-
-        # Union them.
-        self._nodes[parent] += self._nodes[child]
-        self._nodes[child] = parent
-
-        return True
-
-    def _find_parent(self, node):
-        """Finds the parent node for the passed in node.
-
-        If the node does not exist it is created.
-
-        :param node: The node to lookup.
-
-        :return: The parent node of the passed in node.
-        :rtype: node
-        """
-        if node not in self._nodes:
-            self._nodes[node] = 1
-            return node
-        elif isinstance(self._nodes[node], int):
-            return node
-        else:
-            return self._find_parent(self._nodes[node])
 
 
 def kruskal(graph):
@@ -101,14 +21,66 @@ def kruskal(graph):
     edges = list(edges)
     edges.sort(key=lambda x: x[1])
 
-    ds = DisjointSet()
+    cd = _CycleDetector()
     path = []
     total_weight = 0
     for (node1, node2), weight in edges:
-        if ds.union(node1, node2):
+        if not cd.forms_cycle(node1, node2):
             path.append([node1, node2])
             total_weight += weight
     return path, total_weight
 
 
+
+
+class _CycleDetector:
+    def __init__(self):
+        self._parents = {}
+
+    def forms_cycle(self, n1, n2):
+        self._add_nodes_if_needed(n1, n2)
+
+        p1 = self._get_top_level_parent(n1)
+        p2 = self._get_top_level_parent(n2)
+
+        if p1 == p2:
+            return True
+
+        assert self.is_top_level_parent(p1)
+        assert self.is_top_level_parent(p2)
+
+        size_1 = self._parents[p1]
+        size_2 = self._parents[p2]
+
+        if size_1 < size_2:
+            p1, p2 = p2, p1
+            n1, n2 = n2, n1
+
+        self._parents[p1] += size_2
+
+        self._parents[p2] = p1
+        self._parents[n2] = p1
+
+        return False
+
+    @property
+    def parents(self):
+        return self._parents.copy()
+
+    def is_top_level_parent(self, node):
+        assert node in self._parents
+        return isinstance(self._parents[node], int)
+
+    def _add_nodes_if_needed(self, *nodes):
+        for node in nodes:
+            assert not isinstance(node, int)
+            if node not in self._parents:
+                self._parents[node] = 1
+
+    def _get_top_level_parent(self, node):
+        assert node in self._parents
+        if self.is_top_level_parent(node):
+            return node
+        else:
+            return self._get_top_level_parent(self._parents[node])
 
