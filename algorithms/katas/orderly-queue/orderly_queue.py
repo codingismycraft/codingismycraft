72ea5c24652708e5a7c1080e993f58cbac4b1ac0
jpazarzis <jpazarzis@rapidsos.com>
2021-09-06 00:39:16 -0400

    Tests are passing.

--- algorithms/katas/orderly-queue/orderly_queue.py
+++ algorithms/katas/orderly-queue/orderly_queue.py
@@ -6,24 +6,32 @@ https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/
 import heapq
 
 
+def get_index_to_delete(array, low, hi, char_to_add):
+    min_index = low
+    min_value = array[low]
+
+    for i in range(low, hi):
+        if array[i] == char_to_add:
+            continue
+        if array[i] < min_value:
+            min_index = i
+            min_value = array[i]
+
+    return min_index
+
+
 def solve_it(s, k):
     heap = list(s)
     heapq.heapify(heap)
-    response = str(s)
+    response = list(str(s))
 
     for index in range(0, k):
-        print(response)
-        char = heapq.heappop(heap)
-        j = response.index(char, index)
+        char_to_add = heapq.heappop(heap)
+        while response[index] != char_to_add:
+            i = get_index_to_delete(response, index, k, char_to_add)
+            char_to_move = response[i]
+            del response[i]
+            response += [char_to_move]
 
-        a = response[0:index]
-        b = response[j:]
-        c = response[index:j]
-        d = ''.join(sorted(list(response[index:k -index])))
-        e = response[k -index:j]
-
-
-        response = response[0:index] + response[j:] + response[index:j]
-    print(response)
-    return response
+    return ''.join(response)
 
--- algorithms/katas/orderly-queue/test_orderly_queue.py
+++ algorithms/katas/orderly-queue/test_orderly_queue.py
@@ -8,19 +8,24 @@ import orderly_queue
 class TestOrderlyQueue(unittest.TestCase):
     """Test the solve it function."""
 
+    def test_solve_it1(self):
+        retieved = orderly_queue.newString("gaurang", 3)
+        self.assertEqual("agangru", retieved)
+
     def test_solve_it(self):
         """Test the solve it function."""
-        # retieved = orderly_queue.solve_it("badckm", 3)
-        #
-        # print(retieved)
-        #
-        # retieved = orderly_queue.solve_it("cba", 1)
-        # self.assertEqual(retieved, "acb")
-        #
-        # retieved = orderly_queue.solve_it("baaca", 3)
-        # self.assertEqual(retieved, "aaabc")
+        retieved = orderly_queue.solve_it("badckm", 3)
+
+        print(retieved)
+
+        retieved = orderly_queue.solve_it("cba", 1)
+        self.assertEqual(retieved, "acb")
+
+        retieved = orderly_queue.solve_it("baaca", 3)
+        self.assertEqual(retieved, "aaabc")
 
         retieved = orderly_queue.solve_it("xmvzi", 2)
+
 
         bad = "imvzx"
         self.assertEqual("imvxz", retieved)
