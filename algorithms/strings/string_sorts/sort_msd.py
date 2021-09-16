4b151a699e81e1956a8bd91132cb13456c02a2f0
jpazarzis <jpazarzis@rapidsos.com>
2021-09-16 10:29:06 -0400

    Completes msd sorting.

--- algorithms/strings/string_sorts/sort_msd.py
+++ algorithms/strings/string_sorts/sort_msd.py
@@ -1,21 +1,44 @@
 """Sorts a list of strings by Most Significant Digit."""
 
-ALPHABET_SIZE = 27
+ALPHABET_SIZE = 256
 
 
-def get_chr_ord(c):
-    y = ord(c) - ord('a') + 1
+def _get_chr_ord(string, position):
+    """Returns the char index for the postion passed.
+
+    :param str string: The string to look up.
+    :param int position: The char index to use.
+
+    :return: The index of the char in the passed in postion.
+    :rtype: int
+    """
+    if position >= len(string):
+        return 1
+    c = string[position]
+    y = ord(c) - 10 + 2
     return y
 
 
-def msd_sort(strings, position, low_index, high_index):
+def msd_sort(strings, position=0, low_index=None, high_index=None):
+    """Sorts in ascending order and in place the passed in array of strings.
+
+    :param list strings: The list of the strings to sort.
+    :param int position: The char position to sort by.
+    :param int low_index: The low index to apply the sorting for.
+    :param int high_index: The high index to apply the sorting for.
+    """
+    if low_index is None:
+        low_index = 0
+    if high_index is None:
+        high_index = len(strings)
+
     if high_index <= low_index or high_index - low_index == 1:
         return
 
     freqs = [0] * ALPHABET_SIZE
 
     for i in range(low_index, high_index):
-        freqs[get_chr_ord(strings[i][position])] += 1
+        freqs[_get_chr_ord(strings[i], position)] += 1
 
     starting_index = [low_index] * ALPHABET_SIZE
     for i in range(1, len(starting_index)):
@@ -25,7 +48,7 @@ def msd_sort(strings, position, low_index, high_index):
 
     for i in range(low_index, high_index):
         s = strings[i]
-        c = get_chr_ord(s[position])
+        c = _get_chr_ord(s, position)
         index_to_place_s = starting_index[c]
         aux[index_to_place_s] = s
         starting_index[c] += 1
@@ -36,4 +59,5 @@ def msd_sort(strings, position, low_index, high_index):
     for i in range(len(starting_index) - 1):
         from_index = starting_index[i]
         to_index = starting_index[i + 1]
-        msd_sort(strings, position+1, low_index=from_index, high_index=to_index)
+        msd_sort(strings, position + 1, low_index=from_index,
+                 high_index=to_index)
--- algorithms/strings/string_sorts/test_sort_msd.py
+++ algorithms/strings/string_sorts/test_sort_msd.py
@@ -8,156 +8,23 @@ import sort_msd
 class TestSortingByMSD(unittest.TestCase):
     """Tests sorting by most significant digit."""
 
-
-    def test1(self):
-        data = ['acc', 'adb', 'bad', 'bae', 'dda']
-        position = 1
-        low_index = 2
-        high_index = 4
-        strings = data[:]
-        sort_msd.msd_sort(strings, position=position, low_index=low_index,
-                          high_index=high_index)
-
-        print(strings)
-
     def test_count_frequencies(self):
         data = [
             'dda',
-            'adb',
+            'adb  ',
             'acc',
-            'zab',
+            'zab   ',
+            'zaasas',
             'bad',
-            'aaa',
+            'aaaab ',
+            'aaak',
+            'aaaabcddd',
             'bae',
         ]
         strings = data[:]
-        sort_msd.msd_sort(strings, position=0, low_index=0, high_index=len(data))
-
-        print(strings)
-
-
-
-    # def test1(self):
-    #     data = [
-    #         "b",
-    #         "a",
-    #     ]
-    #     expected = ["a", "b"]
-    #     retrieved = sort_msd.sort_by_msd(data, 1)
-    #     self.assertListEqual(retrieved, expected)
-    #     print(retrieved)
-    #
-    # def test2(self):
-    #     data = [
-    #         "ba",
-    #         "aa",
-    #     ]
-    #     expected = ["aa", "ba"]
-    #     retrieved = sort_msd.sort_by_msd(data, 2)
-    #     self.assertListEqual(retrieved, expected)
-    #     print(retrieved)
-
-    # def test3(self):
-    #     data = [
-    #         "bc",
-    #         "ba",
-    #     ]
-    #     expected = ["ba", "bc"]
-    #     retrieved = sort_msd.sort_by_msd(data, 1)
-    #     self.assertListEqual(retrieved, expected)
-    #     print(retrieved)
-
-    # def test4(self):
-    #     data = [
-    #         "bc",
-    #         "ba",
-    #     ]
-    #     expected = ["ba", "bc"]
-    #     retrieved = sort_msd.sort_by_msd(data, 1)
-    #     self.assertListEqual(retrieved, expected)
-    #     print(retrieved)
-
-
-    def test_starting_positions(self):
-        # data = [
-        #     "acc",
-        #     "klx",
-        #     "abc",
-        #     "kac",
-        # ]
-        return
-        data = [
-            "b",
-            "a",
-        ]
-        char_index = 0
-        retrieved = sort_msd.sort_by_msd(data)
-        print(retrieved)
-
-
-        # sorted_data = sorted(data)
-        #
-        # expected_positions = {}
-        # for index, string in enumerate(sorted_data):
-        #     c = ord(string[char_index])
-        #     if c not in expected_positions:
-        #         expected_positions[c] = index
-        #
-        # for c, p in expected_positions.items():
-        #     print(chr(c), p)
-        #     self.assertEqual(sorted_data[p][0], chr(c))
-        #
-        # print(sorted_data)
-
-    # def test_sort_by_msd_1(self):
-    #     """Tests the sort_by_msd fucntion."""
-    #     data = [
-    #         "abc",
-    #         "klx",
-    #         "acc",
-    #     ]
-    #     retrieved = sort_msd.sort_by_msd(data)
-    #     expected = None
-    #     self.assertEqual(retrieved, expected)
-    #
-    # def test_sort_by_msd(self):
-    #     """Tests the sort_by_msd fucntion."""
-    #     data = [
-    #         "4PGC938",
-    #         "2IYE230",
-    #         "3CIO720",
-    #         "1ICK750",
-    #         "1OHV845",
-    #         "4JZY524",
-    #         "1ICK750",
-    #         "3CIO720",
-    #         "1OHV845",
-    #         "1OHV845",
-    #         "2RLA629",
-    #         "2RLA629",
-    #         "3ATW723",
-    #
-    #     ]
-    #
-    #     expected = [
-    #         "1ICK750",
-    #         "1ICK750",
-    #         "1OHV845",
-    #         "1OHV845",
-    #         "1OHV845",
-    #         "2IYE230",
-    #         "2RLA629",
-    #         "2RLA629",
-    #         "3ATW723",
-    #         "3CIO720",
-    #         "3CIO720",
-    #         "4JZY524",
-    #         "4PGC938",
-    #     ]
-    #
-    #     retrieved = sort_msd.sort_by_msd(data)
-    #     expected = None
-    #     self.assertEqual(retrieved, expected)
+        sort_msd.msd_sort(strings)
+        expected = sorted(data)
+        self.assertListEqual(expected, strings)
 
 
 if __name__ == '__main__':
